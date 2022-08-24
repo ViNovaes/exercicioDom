e599844c153bffc9ee0dbe40856e28de5d325c9c
ViNovaes <victoria.novaes07@hotmail.com>
2022-08-24 11:40:33 -0300

    First commit

---
+++ Exercicio 1/exemplo.js
@@ -0,0 +1,11 @@
+function clicou(){
+    document.querySelector('#thanos').innerHTML = "Clicou aqui"
+}
+
+function entrou(){
+    document.querySelector('#thanos').innerHTML = "Passou o mouse"
+}
+
+function fora(){
+    document.querySelector('#thanos').innerHTML = "Tirou o mouse"
+}
---
+++ Exercicio 2/Exercicio2.js
@@ -0,0 +1,6 @@
+function soma() {
+    let n1 = document.getElementById("numero1").value
+    let n2 = document.getElementById("numero2").value
+    document.querySelector("#resultado").innerHTML = (parseInt (n1)) + (parseInt (n2))
+
+}
